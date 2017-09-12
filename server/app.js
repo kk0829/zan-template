3a526e5ab70131f6eb40a6a19b017f16ba209480
kk <kk@youzan.com>
2017-09-12 14:50:53 +0800

    feat: 简化 app.js 启动配置项

--- server/app.js
+++ server/app.js
@@ -1,37 +1,9 @@
 import Zan from 'zan-node';
-import middlewares from './middlewares';
-import auth from './policies/auth';
 
-/**
- * CDN_PATH 静态资源 CDN 域名
- * VERSION_LIST version 文件配置列表
- */
 const config = {
-    SERVER_ROOT: __dirname,
-    NODE_PORT: 8201,
     STATIC_PATH: '../static/local/',
     KEYS: ['your-key', 'your-secret'],
-    CDN_PATH: '//b.yzcdn.cn',
-    VERSION_LIST: [
-        './config/version_css.json',
-        './config/version_h5_css.json',
-        './config/version_js.json',
-        './config/version_h5_js.json'
-    ],
-    beforeLoadMiddlewares() {
-        this.middlewares.push({
-            name: '_global',
-            fn: middlewares._global
-        });
-        this.middlewares.push({
-            name: 'auth',
-            fn: auth
-        });
-        this.middlewares.push({
-            name: 'fail',
-            fn: middlewares.fail
-        });
-    }
+    CDN_PATH: '//b.yzcdn.cn'
 };
 
 let zan = new Zan(config);
--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,7 +1,9 @@
 import _global from './_global';
+import auth from '../policies/auth';
 import fail from './fail';
 
-module.exports = {
+module.exports = [
     _global,
+    auth,
     fail
-};
+];
