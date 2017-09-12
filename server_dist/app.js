a22f18810305cdd37f7e6dcc00d3e7644969325e
kk <kk@youzan.com>
2017-09-12 14:49:49 +0800

    refactor: 删除多余代码

--- server_dist/app.js
+++ server_dist/app.js
@@ -4,41 +4,12 @@ var _zanNode = require('zan-node');
 
 var _zanNode2 = _interopRequireDefault(_zanNode);
 
-var _middlewares = require('./middlewares');
-
-var _middlewares2 = _interopRequireDefault(_middlewares);
-
-var _auth = require('./policies/auth');
-
-var _auth2 = _interopRequireDefault(_auth);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/**
- * CDN_PATH 静态资源 CDN 域名
- * VERSION_LIST version 文件配置列表
- */
 const config = {
-    SERVER_ROOT: __dirname,
-    NODE_PORT: 8201,
     STATIC_PATH: '../static/local/',
     KEYS: ['your-key', 'your-secret'],
-    CDN_PATH: '//b.yzcdn.cn',
-    VERSION_LIST: ['./config/version_css.json', './config/version_h5_css.json', './config/version_js.json', './config/version_h5_js.json'],
-    beforeLoadMiddlewares() {
-        this.middlewares.push({
-            name: '_global',
-            fn: _middlewares2.default._global
-        });
-        this.middlewares.push({
-            name: 'auth',
-            fn: _auth2.default
-        });
-        this.middlewares.push({
-            name: 'fail',
-            fn: _middlewares2.default.fail
-        });
-    }
+    CDN_PATH: '//b.yzcdn.cn'
 };
 
 let zan = new _zanNode2.default(config);
--- server_dist/middlewares/index.js
+++ server_dist/middlewares/index.js
@@ -4,13 +4,14 @@ var _global2 = require('./_global');
 
 var _global3 = _interopRequireDefault(_global2);
 
+var _auth = require('../policies/auth');
+
+var _auth2 = _interopRequireDefault(_auth);
+
 var _fail = require('./fail');
 
 var _fail2 = _interopRequireDefault(_fail);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = {
-    _global: _global3.default,
-    fail: _fail2.default
-};
+module.exports = [_global3.default, _auth2.default, _fail2.default];
